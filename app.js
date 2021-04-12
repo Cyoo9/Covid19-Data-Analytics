9d3560ee14c50537d1131e9c9114b80732562aaf
Caleb Yoo <calebyoo33@gmail.com>
2021-04-12 15:00:49 -0700

    app.js skeleton

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const server = 5000;
+var bodyParser = require("body-parser");
+var multer  = require('multer');
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+});
+
+app.listen(server, function() {
+    console.log(`Server is running on port: ${server}`);
+})
