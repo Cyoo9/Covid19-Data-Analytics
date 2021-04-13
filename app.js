eaa32058639b35255e4141fc01fa07710da6e98c
Caleb Yoo <calebyoo33@gmail.com>
2021-04-12 22:19:39 -0700

    parsing works. need to implement search algo. added a sample get function

--- app.js
+++ app.js
@@ -30,11 +30,33 @@ function csvParser(csv){
   }
   
   let json = JSON.stringify(result); //JSON
+  let js = JSON.parse(json);
+  console.log(getObjects(js, 'UID', '84001133')); //specific search 
   fs.writeFileSync('output.json', json); 
 }
 
-csvParser(csv);
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));    
+        } else 
+        //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+        if (i == key && obj[i] == val || i == key && val == '') { //
+            objects.push(obj);
+        } else if (obj[i] == val && key == ''){
+            //only add if the object is not already in the array
+            if (objects.lastIndexOf(obj) == -1){
+                objects.push(obj);
+            }
+        }
+    }
+    return objects;
+}
 
+csvParser(csv); //test it
+ 
 /* app.use(express.urlencoded({
   extended: true
 }))
