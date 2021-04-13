794ff64f96f82b13bd79af4636e529bc4ce16c71
Caleb Yoo <calebyoo33@gmail.com>
2021-04-12 18:51:29 -0700

    list'

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const app = express();
 const server = 5000;
 var bodyParser = require("body-parser");
 var multer  = require('multer');
+var List = require("collections/list"); //used for csv after array
 
+// var list = new List([])
+
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res) {
+  
 });
 
 app.listen(server, function() {
