96f096b410d50343b1d59a56ab7e4ed7f958f2f1
TheHenn17 <44244825+TheHenn17@users.noreply.github.com>
2021-04-14 09:30:36 -0700

    CSV Parser test

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const app = express();
 const server = 5000;
 var bodyParser = require("body-parser");
 var multer  = require('multer');
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+
+//this works like a charm; i moved the csv file outside the folder bc i didnt know how to locate it
+fs.createReadStream('covid_19_data.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+  });
 
 
 app.get("/", function(req, res) {
@@ -10,6 +21,7 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
+
 });
 
 app.listen(server, function() {
