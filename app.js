11548cbfa486f1189093e946f407e4b4d98d0621
TheHenn17 <44244825+TheHenn17@users.noreply.github.com>
2021-04-14 09:42:30 -0700

    nevermind

--- app.js
+++ app.js
@@ -3,18 +3,6 @@ const app = express();
 const server = 5000;
 var bodyParser = require("body-parser");
 var multer  = require('multer');
-const csv = require('csv-parser')
-const fs = require('fs')
-const results = [];
-
-//this works like a charm; i moved the csv file outside the folder bc i didnt know how to locate it
-fs.createReadStream('covid_19_data.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    console.log(results);
-  });
-
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/covidData.html");
