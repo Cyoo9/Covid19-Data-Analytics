2ab25e99ff6b5a55e1ce1c65f69c118df6f9d5d8
TheHenn17 <44244825+TheHenn17@users.noreply.github.com>
2021-04-19 10:43:02 -0700

    General cleanup

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-
 const express = require('express');
 const app = express();
 const fs = require('fs');
 const server = 5000;
-const path = require ('path');
+const path = require('path');
 let ejs = require('ejs');
-
 
 var bodyParser = require("body-parser");
 var multer  = require('multer');
-var csvFileName = "covid_19_data.csv"; //just testing the functions
-var csv = fs.readFileSync(csvFileName); //reads in a cvs file
+var csv = fs.readFileSync(path.resolve(__dirname, './CSV Files/covid_19_data.csv')); //reads in a cvs file
 var result = [];
 let searched_results = [];
 
@@ -37,11 +34,8 @@ function csvParser(csv){
 
 csvParser(csv);
 
+//Reformats dates from something like 12/19/2020 to 2020-12-19
 function ReformatDate(old_date) {
-
-  //Example
-  //old: 12/19/2020
-  //new: 2020-12-19
 
   if (old_date.length == 10) {
     return `${old_date.substr(6, 4)}-${old_date.substr(0, 2)}-${old_date.substr(3, 2)}`;
@@ -49,61 +43,27 @@ function ReformatDate(old_date) {
   else {
     return "Unknown Date";
   }
-  
-
 }
-
-
 
 //searchs an array for specific object values
 function search(req, res, next) {
- 
-  //var search = []; //create array that will hold the objects with the correct search values
-
-  //console.log(req.body.confirmedCases);
-  //console.log(req.body.deaths);
-  //console.log(req.body.recoveries);
   
-
-  searched_results = []; //reset
+  searched_results = []; //reset array to empty
 
   //loops through all the arrays objects
   for(var i = 0; i < result.length; i++) {
     //checks if country, state, and date match, accepts all if one or more is left blank
     if(result[i]['Country/Region'] == req.body.country || req.body.country == '') {
-
-
       if(result[i]['Province/State'] == req.body.state || req.body.state == '') {
-
-        const reformatted_date = ReformatDate(result[i]['ObservationDate']);
-        //result[i]['ObservationDate'] = ReformatDate(result[i]['ObservationDate']);
-
+        const reformatted_date = ReformatDate(result[i]['ObservationDate']); //use reformatted date
         if(reformatted_date == req.body.date || req.body.date == '') {
           searched_results.push(result[i]);
-           //console.log(i);
         }
       }
     }
   }
-
-
   next();
 }
-
-
-/*
-
-    ObservationDate: '01/23/2020',
-    'Province/State': 'Xinjiang',
-    'Country/Region': 'Mainland China',
-    'Last Update': '1/23/20 17:00',
-    Confirmed: '2.0',
-    Deaths: '0.0',
-    Recovered: '0.0'
-
-*/
-
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -111,56 +71,32 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, "/public" , "covidData.html"));
 });
 
-
 app.get('/output.json', (req, res) => {
-  console.log('xd');
-  res.send("XDDDDD");
+  console.log('xd');  //??
+  res.send("XDDDDD");  //??
   //res.sendFile(path.join(__dirname, "/public" , "output.json"));
 });
-
-
 
 app.use(express.urlencoded({
   extended: true
 }))
 
-
-// var btn = document.getElementById("btn");
-// var ourRequest = new XMLHttpRequest();
-// ourRequest.open('GET', './public/output.json')
-// ourRequest.onload = function() {
-//   var ourData = JSON.parse(ourRequest.responseText);
-//   console.l
-// }
-
-
-// btn.addEventListener("click", function() {
-  
-// })
-
-
-
-
-//app.use(search); //middleware
-
+//calls the middlewear function "search" after the client posts a request
 app.post('/', search,  (req, res) => {
-  //console.log( "XDXD XDXD: " + __dirname + "/public/output.json");
 
-  console.log(req.body);
+  /*console.log(req.body);
 
   console.log(searched_results.length);
   for (let i = 0; i < searched_results.length; ++i) {
     console.log(`${i}: ${JSON.stringify(searched_results[i])}`);
-  }
+  } */
   
   let json = JSON.stringify(searched_results); //stringify the search array
   fs.writeFileSync('./public/output.json', json); //store the string in a json file to be sent to front-end
 
-  //res.end();
   res.sendFile(path.join(__dirname, "/public" , "output.json"));
-
+  //res.end();
 });
-
 
 app.listen(server, function() {
     console.log(`Server is running on port: ${server}`);
